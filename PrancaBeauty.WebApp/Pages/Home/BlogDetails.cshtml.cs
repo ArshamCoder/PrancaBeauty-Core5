be498ee09b604e69951b93aae4c02565c3310585
Arsham Behbahani <behbahani_arsham@hotmail.com>
2021-01-21 12:08:26 +0330

    Add project files.

---
+++ PrancaBeauty.WebApp/Pages/Home/BlogDetails.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BeautyShop.Pages.Home
+{
+    public class BlogDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
