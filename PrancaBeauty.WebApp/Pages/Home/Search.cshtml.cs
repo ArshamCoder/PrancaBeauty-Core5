9d484fb3b24023bcd477d9ffd7d11e64498ca373
Arsham Behbahani <behbahani_arsham@hotmail.com>
2021-01-23 12:47:48 +0330

    22 پایان - لایه بندی

--- PrancaBeauty.WebApp/Pages/Home/Search.cshtml.cs
+++ PrancaBeauty.WebApp/Pages/Home/Search.cshtml.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace BeautyShop.Pages.Home
+namespace PrancaBeauty.WebApp.Pages.Home
 {
     public class SearchModel : PageModel
     {
