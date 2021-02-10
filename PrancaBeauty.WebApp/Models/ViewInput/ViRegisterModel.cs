8178625dbd837a086c948c013af7f0913404f2e8
Arsham Behbahani <behbahani_arsham@hotmail.com>
2021-02-10 20:39:47 +0330

    جلسه 38

---
+++ PrancaBeauty.WebApp/Models/ViewInput/ViRegisterModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrancaBeauty.WebApp.Models.ViewInput
+{
+    public class ViRegisterModel
+    {
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "PhoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "FirstName")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "ConfirmPassword")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ PrancaBeauty.WebApp/Pages/Auth/Register.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PrancaBeauty.WebApp.Models.ViewInput;
+
+namespace PrancaBeauty.WebApp.Pages.Auth
+{
+    public class RegisterModel : PageModel
+    {
+        public ViRegisterModel Input { get; set; }
+        public RegisterModel()
+        {
+
+        }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}
