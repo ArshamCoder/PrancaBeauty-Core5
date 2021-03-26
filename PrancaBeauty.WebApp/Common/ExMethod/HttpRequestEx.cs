af6ede241017932ef55ed30934059909e4d5be8b
Arsham Behbahani <behbahani_arsham@hotmail.com>
2021-03-27 02:00:24 +0430

    جلسه 68

---
+++ PrancaBeauty.WebApp/Common/ExMethod/HttpRequestEx.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace PrancaBeauty.WebApp.Common.ExMethod
+{
+    public static class HttpRequestEx
+    {
+        public static string GetCurrentUrl(this HttpRequest request)
+        {
+            string url = request.Scheme + "://" + request.Host + request.Path;
+            if (request.QueryString.HasValue)
+                url += request.QueryString.Value;
+
+            return url;
+        }
+    }
+}
