08b11b9838b51b2ce9e1b3756757fa73dcc513c2
Arsham Behbahani <behbahani_arsham@hotmail.com>
2021-06-30 17:18:22 +0430

    جلسه 122

---
+++ PrancaBeauty.WebApp/Models/ViewModel/ViCompo_Area.cs
@@ -0,0 +1,14 @@
+﻿namespace PrancaBeauty.WebApp.Models.ViewModel
+{
+    public class ViCompo_Area
+    {
+        public string CountryId { get; set; }
+        public string CountyFieldName { get; set; }
+
+        public string ProvinceId { get; set; }
+        public string ProvinceFieldName { get; set; }
+
+        public string CityId { get; set; }
+        public string CityFieldName { get; set; }
+    }
+}
---
+++ PrancaBeauty.WebApp/Pages/Shared/Components/Region/Combo_Country/Compo_Combo_Countries.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PrancaBeauty.Application.Apps.Countries;
+using PrancaBeauty.WebApp.Models.ViewInput;
+using PrancaBeauty.WebApp.Models.ViewModel;
+
+namespace PrancaBeauty.WebApp.Pages.Shared.Components.Region.Combo_Country
+{
+    public class Compo_Combo_CountriesModel : PageModel
+    {
+        private readonly IMapper _Mapper;
+        private readonly ICountryApplication _CountryApplication;
+        public Compo_Combo_CountriesModel(ICountryApplication countryApplication, IMapper mapper)
+        {
+            _CountryApplication = countryApplication;
+            _Mapper = mapper;
+        }
+
+        public IActionResult OnGet()
+        {
+            if (Input.FieldName == null)
+                Input.FieldName = "Input.CountryId";
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnGetReadAsync(string LangId, string Text)
+        {
+            var qData = await _CountryApplication.GetListForComboAsync(LangId, Text);
+            var Data = _Mapper.Map<List<vmCompo_Combo_Countries>>(qData);
+            return new JsonResult(Data);
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public viCompo_Combo_Countries Input { get; set; }
+    }
+}
