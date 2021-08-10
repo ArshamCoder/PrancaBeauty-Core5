c90b0311b4a6e17c2cf9cb9eae06622eb6abb649
Arsham Behbahani <behbahani_arsham@hotmail.com>
2021-08-10 23:18:03 +0430

    جلسه 148

--- Framework/Application/Services/Security/AntiShell/AniShell.cs
+++ Framework/Application/Services/Security/AntiShell/AniShell.cs
@@ -65,6 +65,7 @@ namespace Framework.Application.Services.Security.AntiShell
                     hex += string.Format("{0:X}", item) + " ";
                 }
 
+
                 if (hex.StartsWith("89 50 4E 47 0D 0A 1A 0A"))
                 {
                     return ("png", "image/png");
